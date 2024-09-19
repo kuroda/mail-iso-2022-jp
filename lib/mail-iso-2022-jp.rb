240d52d713d2626201b3f47c957de5de70c1e996
Benjamin Quorning <benjamin@quorning.net>
2024-09-19 17:15:07 +0200

    Explicitly drop support below Ruby 2.2
    Commit 3cb86ffdf51ee28ba47c3f8bf82d07684133e621 announced that support
    for Ruby 2.1 was dropped. This commit removes code related to Ruby 1.8,
    and specifies in gemspec that Ruby >= 2.2 must be used.
    
    Ruby 2.1 was EOL 7 years ago, so I hope this is ok.

--- lib/mail-iso-2022-jp.rb
+++ lib/mail-iso-2022-jp.rb
@@ -3,17 +3,11 @@ require 'mail-iso-2022-jp/invalid_encoding_error'
 require 'mail-iso-2022-jp/common_methods_for_field'
 require 'mail-iso-2022-jp/preprocessor'
 
-if RUBY_VERSION >= '1.9'
-  require 'mail-iso-2022-jp/mail'
-  require 'mail-iso-2022-jp/message'
-  require 'mail-iso-2022-jp/field'
-  require 'mail-iso-2022-jp/field_with_iso_2022_jp_encoding'
-  require 'mail-iso-2022-jp/body'
-else
-  require 'mail-iso-2022-jp/ruby18/mail'
-  require 'mail-iso-2022-jp/ruby18/message'
-  require 'mail-iso-2022-jp/ruby18/field_with_iso_2022_jp_encoding'
-end
+require 'mail-iso-2022-jp/mail'
+require 'mail-iso-2022-jp/message'
+require 'mail-iso-2022-jp/field'
+require 'mail-iso-2022-jp/field_with_iso_2022_jp_encoding'
+require 'mail-iso-2022-jp/body'
 
 require 'mail-iso-2022-jp/header'
 require 'mail-iso-2022-jp/subject_field'
